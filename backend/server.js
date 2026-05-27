8299d47dcfbde42670ca2072e524068e4cdaa0b4
Shubhi Gulati <shubhigulati2534@gmail.com>
2026-05-27 10:16:49 +0530

    Hardcode Vercel origin for CORS

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 const frontendURL = process.env.FRONTEND_URL || "http://localhost:5173";
 app.use(
   cors({
-    origin: [frontendURL, "http://localhost:5173", "http://localhost:5174"],
+    origin: [frontendURL, "http://localhost:5173", "http://localhost:5174", "https://major-project-xi-opal.vercel.app"],
     credentials: true,
   })
 );
